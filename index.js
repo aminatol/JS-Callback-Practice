93fb9a5c3de2326b2de7eb3b53ff0a23ef8c2dce
aminatol <aminatoluwo@gmail.com>
2023-06-05 22:23:59 -0400

    completed handlec change direction function

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ let direction = null;
 let x = 100;
 let y = 250;
 
-function moveCharacter() {
+function handleDirectionChange(direction) {
+  if (direction === null) {
+    character.src = "assets/green-character/static.gif";
+  }
+  if (direction === "west") {
+    character.src = "assets/green-character/west.gif";
+  }
+  if (direction === "north") {
+    character.src = "assets/green-character/north.gif";
+  }
+  if (direction === "east") {
+    character.src = "assets/green-character/east.gif";
+  }
+  if (direction === "south") {
+    character.src = "assets/green-character/south.gif";
+  }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange);
+
+setInterval(function () {
   if (direction === "west") {
     x = x - 1;
   }
@@ -18,8 +38,27 @@ function moveCharacter() {
   }
   character.style.left = x + "px";
   character.style.bottom = y + "px";
-}
-setInterval(moveCharacter, 1);
+}, 1);
+
+document.addEventListener("keydown", function (e) {
+  if (e.repeat) return;
+  if (e.key === "ArrowLeft") {
+    direction = "west";
+  }
+  if (e.key === "ArrowUp") {
+    direction = "north";
+  }
+  if (e.key === "ArrowDown") {
+    direction = "south";
+  }
+  if (e.key === "ArrowRight") {
+    direction = "east";
+  }
+});
+
+document.addEventListener("keyup", function (e) {
+  direction = null;
+});
 
 const inventory = newInventory();
 move(inventory).to(0, 0);
